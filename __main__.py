909fa24af882ae0ba66204b595dc695540405308
L. Kärkkäinen <tronic@users.noreply.github.com>
2019-07-11 16:09:20 +0300

    Remove debug

--- __main__.py
+++ __main__.py
@@ -1,7 +1,7 @@
 from .app import app
 import os
 
-debug = True #os.getenv("DEBUG", "") == "1"
+debug = os.getenv("DEBUG", "") == "1"
 app.config.SERVER_NAME = "http://localhost:8008" if debug else "https://paste.zi.fi"
 app.config.REQUEST_MAX_SIZE = 100_000
 app.run(host="127.0.0.1", port=8008, debug=debug, auto_reload = os.name == "posix")
